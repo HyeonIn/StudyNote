578218f0133f1dfe802d424b7edfd667c215abd0
HyeonIn <gusdlsdlek@gmail.com>
2022-11-08 13:56:15 +0900

    Front-End: Vue Study

---
+++ Front-End/VueJS/LAB/1108/LAB/SFC/components/bookMain.js
@@ -0,0 +1,9 @@
+export default {
+  template: `
+  <div>
+  <ul>
+    <li><a href="./create.html">도서 등록</a></li>
+    <li><a href="./list.html">도서 목록</a></li>
+  </ul>
+</div>`,
+};
---
+++ Front-End/VueJS/LAB/1108/LAB/SFC/components/headerNav.js
@@ -0,0 +1,7 @@
+export default {
+  template: `
+  <div class="header">
+    <a href="index.html"> <img src="./img/ssafy_logo.png" class="ssafy_logo" /></a>
+    <p class="logo">도서관리</p>
+  </div>`,
+};
