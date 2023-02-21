9ca82da891a18dc26b0258ca52d4c97ae5f83bd0
HyeonIn <gusdlsdlek@gmail.com>
2023-02-21 10:15:42 +0900

    FrontEnd : JSX, 파일명 변경

--- Front-End/React/Component.md
+++ Front-End/React/Component.md
@@ -1,3 +1,103 @@
+## 4. 컴포넌트 만들기
+
+컴포넌트의 개념을 이해하고 고객 컴포넌트를 만들어서 실습을 진행한다.
+
+### Component
+
+리액트 라이브러리 자체는 HTML 웹 문서를 효과적으로 보여주기 위한 라이브러리이다.
+
+- 컴포넌트는 웹 문서에서 어떠한 내용을 보여주기 위한 기본적인 단위
+- 계층적으로 구성될 수 있음
+- 실제로 웹 문서에서 그려지게 될 최소 단위
+
+<aside>
+💡 src 폴더 안에 components 폴더를 생성해서 컴포넌트들을 모아 놓는다
+
+</aside>
+
+### import / export
+
+- import
+  - 특정한 라이브러리나 컴포넌트를 불러 올 때 사용
+- export
+  - 다른 컴포넌트에서 사용할 수 있도록 해준다
+
+### render()
+
+- 컴포넌트가 실제로 담고있는 내용을 그리기 위한 함수
+- return() 안에 출력될 내용(HTML)을 정의한다
+
+### 컴포넌트 구현 방법
+
+```jsx
+//Customer.js
+
+import React from 'react' //react 라이브러리 사용
+
+//컴포넌트는 계층적인 구조를 가질 수 있음
+//react 라이브러리의 Component를 상속받아서 Customer 컴포넌트를 만듦
+class Customer extends React.Component{ 
+ //클래스 정의
+}
+
+export default Customer; //Customer 컴포넌트를 사용할 때 Customer란 태그로 사용하게 함
+```
+
+```jsx
+//App.js
+import Customer from '[Customer 소스코드 경로]'
+
+..
+class App extends Component{
+ <Customer/> //Customer 컴포넌트를 Export한 이름으로 태그 작성
+}
+..
+```
+
+### Props
+
+- 속성이라는 의미이며, React에서는 상위 컴포넌트에서 보내는 내용을 뜻함
+
+```jsx
+//App.js
+import Customer from '[Customer 소스코드 경로]'
+
+const customer = {
+ 'name'='최현인',
+ 'age'=19,
+}
+..
+class App extends Component{
+ <Customer
+  name={customer.name}
+  age={customer.age}
+ />
+}
+..
+```
+
+```jsx
+//Customer.js
+
+import React from 'react' //react 라이브러리 사용
+
+//this를 이용하여 props에 접근한다.
+//계층적인 구조이기 때문에 상위 컴포넌트인 App에서 정보를 받아올 수 있는데
+//이 때 this를 이용하여 props에 담긴 내용을 접근 가능하다.
+class Customer extends React.Component{ 
+ render() {
+        return(
+            <div>
+                <h2>{this.props.name}</h2>
+                <p>age : {this.props.age}</p>
+            </div>
+        )
+    }
+}
+
+export default Customer; //Customer 컴포넌트를 사용할 때 Customer란 태그로 사용하게 함
+```
+
 ## 5. 컴포넌트 구조화하기
 
 컴포넌트의 내용을 적절히 분리하며 관리하기 쉬워지며 생산성을 높일 수 있다. 따라서 출력할 내용이 많아지면 구조화하여 분리할 필요가 있다.
@@ -11,9 +111,9 @@ class Customer extends React.Component {
     render() {
         return(
             <div>
-								<img src={this.props.image} alt="profile" />
+        <img src={this.props.image} alt="profile" />
                 <h2>{this.props.name}({this.props.id})</h2>
-								<p>{this.props.birthday}</p>
+        <p>{this.props.birthday}</p>
                 <p>{this.props.gender}</p>
                 <p>{this.props.job}</p>
             </div>
@@ -42,7 +142,7 @@ class CustomerProfile extends React.Component{
     render(){
         return (
             <div>
-	              <img src={this.props.image} alt="profile" />
+               <img src={this.props.image} alt="profile" />
                 <h2>{this.props.name}({this.props.id})</h2>  
             </div>
         )
@@ -82,7 +182,7 @@ Customer 컴포넌트를 하위 컴포넌트 두 개로 분리하여 관리한�
 - 예시
 
 ```jsx
-		const customers = [{
+  const customers = [{
   'id' : 1,
   'image' : 'https://placeimg.com/64/64/1',
   'name' : '홍길동',
--- Front-End/React/lab/management/src/App.js
+++ Front-End/React/lab/management/src/App.js
@@ -1,57 +1,59 @@
-import Customer from './components/Customer';
-import './App.css';
-import Table from '@material-ui/core/Table'
-import TableHead from '@material-ui/core/TableHead'
-import TableBody from '@material-ui/core/TableBody'
-import TableRow from '@material-ui/core/TableRow'
-import TableCell from '@material-ui/core/TableCell'
-import Paper from '@material-ui/core/Paper';
-import { withStyles } from '@material-ui/core';
-import React from 'react'
-import Users from './components/Users';
+import Customer from "./components/Customer";
+import "./App.css";
+import Table from "@material-ui/core/Table";
+import TableHead from "@material-ui/core/TableHead";
+import TableBody from "@material-ui/core/TableBody";
+import TableRow from "@material-ui/core/TableRow";
+import TableCell from "@material-ui/core/TableCell";
+import Paper from "@material-ui/core/Paper";
+import { withStyles } from "@material-ui/core";
+import React from "react";
+import Users from "./components/Users";
 
-const styles = theme => ({
+const styles = (theme) => ({
   root: {
-    width: '100%',
+    width: "100%",
     marginTop: theme.spacing.unit * 3,
-    overflowX: "auto"
+    overflowX: "auto",
   },
   table: {
-    minWidth: 1080
-  }
-})
+    minWidth: 1080,
+  },
+});
 
-const customers = [{
-  'id' : 1,
-  'image' : 'https://placeimg.com/64/64/1',
-  'name' : '홍길동',
-  'birthday' : '961222',
-  'gender' : '남자',
-  'job' : '대학생'
-},
-{
-  'id' : 2,
-  'image' : 'https://placeimg.com/64/64/2',
-  'name' : '김삿갓',
-  'birthday' : '951111',
-  'gender' : '남자',
-  'job' : '프로그래머'
-},
-{
-  'id' : 3,
-  'image' : 'https://placeimg.com/64/64/3',
-  'name' : '최현인',
-  'birthday' : '980622',
-  'gender' : '남자',
-  'job' : '취준생'
-}]
+const customers = [
+  {
+    id: 1,
+    image: "https://placeimg.com/64/64/1",
+    name: "홍길동",
+    birthday: "961222",
+    gender: "남자",
+    job: "대학생",
+  },
+  {
+    id: 2,
+    image: "https://placeimg.com/64/64/2",
+    name: "김삿갓",
+    birthday: "951111",
+    gender: "남자",
+    job: "프로그래머",
+  },
+  {
+    id: 3,
+    image: "https://placeimg.com/64/64/3",
+    name: "최현인",
+    birthday: "980622",
+    gender: "남자",
+    job: "취준생",
+  },
+];
 
 class App extends React.Component {
   render() {
     const { classes } = this.props;
     return (
       <Paper className={classes.root}>
-        <Table className={ classes.table}>
+        <Table className={classes.table}>
           <TableHead>
             <TableRow>
               <TableCell>번호</TableCell>
@@ -63,8 +65,7 @@ class App extends React.Component {
             </TableRow>
           </TableHead>
           <TableBody>
-          {
-            customers.map(customer => { 
+            {customers.map((customer) => {
               return (
                 <Customer
                   key={customer.id}
@@ -75,14 +76,12 @@ class App extends React.Component {
                   gender={customer.gender}
                   job={customer.job}
                 />
-              )
-            })
-          }
+              );
+            })}
           </TableBody>
         </Table>
-        <Users/>
+        <Users />
       </Paper>
-      
     );
   }
 }
